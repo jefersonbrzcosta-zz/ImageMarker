dba76a265400d7dc3ae50961c0aa1f26daf570fd
Jeferson Costa <jefersonbrz.costa@gmail.com>
2021-03-06 20:16:00 -0300

    First Commit (Plan component)

---
+++ App.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ImageMarker from './src/pages/ImageMarker';
+
+const App = () => {
+  return <ImageMarker />;
+};
+
+export default App;
---
+++ src/pages/ImageMarker.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function ImageMarker() {
+  return (
+    <View
+      style={{
+        display: 'flex',
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <Text>This is image Marker</Text>
+    </View>
+  );
+}
+
+export default ImageMarker;
