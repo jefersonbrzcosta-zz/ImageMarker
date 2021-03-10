8507352c7ab92c9d2ca898bc554f1360b85778b7
Romulo Costa <romulocosta@MacBook-Pro-de-Romulo-2.local>
2021-03-10 10:18:14 -0300

    romulo

---
+++ src/components/MarkerOnImage.js
@@ -0,0 +1,52 @@
+/* eslint-disable react-native/no-inline-styles */
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function MarkerOnImage({id, selected, scale}) {
+  return (
+    <View
+      style={{
+        position: 'relative',
+        width: 100,
+        height: 100,
+        backgroundColor: 'rgba(255, 255, 255, 0.3)',
+      }}>
+      <View
+        style={{
+          width: '25%',
+          height: '25%',
+          backgroundColor: selected ? 'orange' : 'gray',
+          alignSelf: 'center',
+          marginTop: '35%',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text style={{color: 'white', fontWeight: 'bold'}}>{id}</Text>
+      </View>
+      <View
+        style={{
+          width: '35%',
+          height: '35%',
+          backgroundColor: 'transparent',
+          borderTopWidth: 3,
+          borderRightWidth: 3,
+          position: 'absolute',
+          right: 0,
+        }}
+      />
+      <View
+        style={{
+          width: '35%',
+          height: '35%',
+          backgroundColor: 'transparent',
+          borderBottomWidth: 3,
+          borderLeftWidth: 3,
+          position: 'absolute',
+          bottom: 0,
+        }}
+      />
+    </View>
+  );
+}
+
+export default MarkerOnImage;
--- src/pages/ImageMarker.js
+++ src/pages/ImageMarker.js
@@ -1,5 +1,5 @@
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import {
   View,
   Image,
@@ -10,6 +10,7 @@ import {
 import Gestures from 'react-native-easy-gestures';
 import RoundedButton from '../components/RoundedButton';
 import Marker from '../components/Marker';
+import MarkerOnImage from '../components/MarkerOnImage';
 
 function ImageMarker() {
   const [image, setImage] = useState({
@@ -25,6 +26,7 @@ function ImageMarker() {
   ]);
   const [fixedMarkers, setFixedMarkers] = useState([]);
   const [markerStyle, setMarkerStyle] = useState();
+  const [imageStyle, setImageStyle] = useState();
   const dimensions = Dimensions.get('window');
   const imageHeight = Math.round((dimensions.width * 9) / 16);
   const imageWidth = dimensions.width;
@@ -54,93 +56,105 @@ function ImageMarker() {
   const fixMarker = (marker) => {
     setFixedMarkers((prevFixMarkers) => [
       ...prevFixMarkers,
-      {marker, style: markerStyle},
+      {marker, styleMarker: markerStyle, styleImage: imageStyle},
     ]);
     setMarkerStyle({});
   };
 
   return (
-    <View
-      style={{
-        display: 'flex',
-        flex: 1,
-        marginTop: 30,
-      }}>
-      <Gestures rotatable={false} draggable={image.move} scalable={image.move}>
-        <TouchableWithoutFeedback
-          onPress={() =>
-            setImage((prev) => ({...prev, selected: !prev.selected}))
-          }>
-          <View>
-            <Image
-              style={{
-                width: imageWidth,
-                height: 300,
-              }}
-              source={{
-                uri:
-                  'https://cdn.pixabay.com/photo/2015/08/02/22/18/barley-872000_960_720.jpg',
-              }}
+    <>
+      <View
+        style={{
+          display: 'flex',
+          flex: 1,
+          // marginTop: 30,
+        }}>
+        <Gestures
+          rotatable={false}
+          draggable={image.move}
+          scalable={image.move}
+          onEnd={(event, styles) => {
+            console.log(styles, 'image');
+            setImageStyle(styles);
+          }}>
+          <TouchableWithoutFeedback
+            onPress={() =>
+              setImage((prev) => ({...prev, selected: !prev.selected}))
+            }>
+            <View>
+              <Image
+                style={{
+                  width: imageWidth,
+                  height: 300,
+                }}
+                source={{
+                  uri:
+                    'https://cdn.pixabay.com/photo/2015/08/02/22/18/barley-872000_960_720.jpg',
+                }}
+              />
+            </View>
+          </TouchableWithoutFeedback>
+          {fixedMarkers?.map((marker, index) => {
+            console.log(marker.styleImage, 'markers');
+            return (
+              <>
+                <Gestures
+                  key={marker.id}
+                  rotatable={marker.move}
+                  draggable={marker.move}
+                  scalable={marker.move}>
+                  <View
+                    style={[
+                      {
+                        position: 'absolute',
+                        left: marker.styleMarker.left - marker.styleImage.left,
+                        top:
+                          marker.styleMarker.top - marker.styleImage.top - 300,
+                      },
+                      {transform: marker.styleMarker.transform},
+                    ]}>
+                    <MarkerOnImage
+                      key={marker.id}
+                      id={marker.id}
+                      selected={marker.selected}
+                      // scale={marker.}
+                    />
+                  </View>
+                </Gestures>
+              </>
+            );
+          })}
+        </Gestures>
+
+        <View style={{position: 'absolute', bottom: 0, marginLeft: 20}}>
+          <RoundedButton
+            title="Image"
+            functionClick={() =>
+              setImage((prev) => ({...prev, move: !prev.move}))
+            }
+            active={image.move}
+          />
+          {markers.map((marker) => (
+            <RoundedButton
+              title="Marker: "
+              functionClick={() => fixMarker(marker)}
+              active={image.move}
             />
-          </View>
-        </TouchableWithoutFeedback>
-        {fixedMarkers?.map((marker) => (
-          <>
-            <Gestures
-              key={marker.id}
-              rotatable={marker.move}
-              draggable={marker.move}
-              scalable={marker.move}>
-              <TouchableWithoutFeedback
-                onPress={() => makeMarkerSelected(marker.id)}>
-                <View
-                  style={[
-                    {
-                      position: 'absolute',
-                      left: imageWidth / 2 + marker.style.left - 60,
-                      top: imageHeight / 2 + marker.style.top - 350,
-                    },
-                    {transform: marker.style.transform},
-                  ]}>
-                  <Marker
-                    key={marker.id}
-                    id={marker.id}
-                    selected={marker.selected}
-                  />
-                </View>
-              </TouchableWithoutFeedback>
-            </Gestures>
-          </>
-        ))}
-      </Gestures>
-      <View>
+          ))}
+        </View>
+      </View>
+      <View style={{position: 'absolute', top: 0}}>
         {markers.map((marker) => (
           <Gestures
             onEnd={(event, styles) => {
-              console.log(styles);
+              console.log(styles, 'marker');
               setMarkerStyle(styles);
             }}>
             <Marker key={marker.id} id={marker.id} selected={marker.selected} />
           </Gestures>
         ))}
       </View>
-      <View style={{position: 'absolute', bottom: 0, marginLeft: 20}}>
-        <RoundedButton
-          title="Image"
-          functionClick={() =>
-            setImage((prev) => ({...prev, move: !prev.move}))
-          }
-          active={image.move}
-        />
-        {markers.map((marker) => (
-          <RoundedButton
-            title="Marker: "
-            functionClick={() => fixMarker(marker)}
-            active={image.move}
-          />
-        ))}
-      </View>
-    </View>
+    </>
   );
 }
 
